c305696a794524d06983c676a1b1abb29c348413
Logan Kottler <logankottler@gmail.com>
2018-04-22 20:38:22 -0500

    did some things

--- src/application/Main.java
+++ src/application/Main.java
@@ -32,8 +32,10 @@ public class Main extends Application {
 	
 	private void menuScreen(Stage primaryStage){
 		
+		primaryStage.setTitle("Tournament Bracket Program: Milestone2"); //TODO
+		
 		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
+		grid.setAlignment(Pos.TOP_CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
@@ -44,16 +46,25 @@ public class Main extends Application {
 		mainMenu.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
 		
 		Text welcomeText = new Text("Welcome to Tournament Builder!");
-		welcomeText.setFont(Font.font("Courier New", FontWeight.NORMAL, 20));
+		welcomeText.setFont(Font.font("Courier New", FontWeight.EXTRA_BOLD, 30));
 		grid.add(welcomeText, 0, 0, 2, 1);
 		
 		Button bracketBtn = new Button("View Bracket");
 		bracketBtn.setOnAction(e -> viewBracket(primaryStage));
+		bracketBtn.setPrefSize(120, 40);
 		HBox hbBracketBtn = new HBox(10);
 		
 		hbBracketBtn.setAlignment(Pos.BOTTOM_RIGHT);
 		hbBracketBtn.getChildren().add(bracketBtn);
-		grid.add(hbBracketBtn, 1, 4);
+		grid.add(hbBracketBtn, 2, 4);
+		
+		Button loadBtn = new Button("Load File");
+		//TODO loadBtn.setOnAction(e -> loadFile(File f));
+		loadBtn.setPrefSize(120, 40);
+		HBox hbLoadBtn = new HBox(10);
+		hbLoadBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		hbLoadBtn.getChildren().add(loadBtn);
+		grid.add(hbLoadBtn, 1, 4);
 		
 		primaryStage.setScene(mainMenu);
 		primaryStage.show();
@@ -63,39 +74,67 @@ public class Main extends Application {
 	
 	public void viewBracket(Stage primaryStage) {
 		
-Pane root = new Pane();
+		Pane root = new Pane();
 		Scene scene1 = new Scene(root, 800, 600);	
 		scene1.getStylesheets().clear();
 		File f = new File("src/application/application.css");		
 		scene1.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
-		
-		primaryStage.setTitle("Bracket");
-		Screen screen = Screen.getPrimary();
-		Rectangle2D bounds = screen.getVisualBounds();
 
-		/*
-		primaryStage.setX(bounds.getMinX());
-		primaryStage.setY(bounds.getMinY());
-		primaryStage.setWidth(bounds.getWidth());
-		primaryStage.setHeight(bounds.getHeight());
-		*/
-		
 		ArrayList<Team> teams = b.getTeams();
 		int spacing = 50;
-		int smallSpace = 5;
+		int smallSpace = 10;
+		
+		
+		int numTeams = b.getSize();
+		int x, y, xDif, yDif;
+		int btnWidth = 90, btnHeight = 30;
+		int ySpace = btnHeight*2;
+		
+		for (int i = 0; i < 31 - Integer.numberOfLeadingZeros(numTeams); i++){
+			int subTeamNum = (i == 0) ? numTeams : numTeams / (2*i);
+			xDif = i*(btnWidth + 10);
+			yDif = btnHeight*(1 << i) - btnHeight;
+			for (int j = 0; j < subTeamNum; j++){
+				Button btn = new Button();
+				btn.setText(teams.get(j).getTeamName()); //TODO correctly name teams
+				btn.setMinSize(btnWidth, btnHeight);
+				int yMul = (i == 0) ? 1 : 1 << i;
+				x = (j < subTeamNum/2) ? 0 + xDif : 505 - xDif;
+				y = (j < subTeamNum/2) 
+						? j*ySpace*yMul + yDif
+						: (j - subTeamNum/2)*ySpace*yMul + yDif;
+				x += 25; //padding
+				y += 25; //padding
+				btn.setLayoutX(x);
+				btn.setLayoutY(y);
+				//TODO btn.setOnAction(e -> setScoreOfTeam());
+				root.getChildren().add(btn);
+				
+			}
+		}
+		/*
 		for (int i = 0; i < b.getSize(); i++){
 			Button butt = new Button(teams.get(i).getTeamName());
-			butt.setMinSize(130, 50);
-			butt.setLayoutX((i >= b.getSize()/2) ? 5 : 305);
+			butt.setMinSize(90, 30);
+			int x = (i >= b.getSize()/2) ? 5 : 505;
+			x += 25; //padding
+			butt.setLayoutX(x);
 			int y = spacing*i - (i%2)*smallSpace;
 			y = (i >= b.getSize()/2) ? y - spacing*b.getSize()/2 : y; //TODO
+			y += 25; //padding
 			butt.setLayoutY(y);
 			root.getChildren().add(butt);
 		}
-			
+		*/
+		Button optionsBtn = new Button("Additional Options");
+		optionsBtn.setOnAction(e -> menuScreen(primaryStage));
+		optionsBtn.setMinSize(120, 40);
+		optionsBtn.setLayoutX(655);
+		optionsBtn.setLayoutY(25);
+		root.getChildren().add(optionsBtn);
+		
 		primaryStage.setScene(scene1);
 		primaryStage.show(); 
-		
 	}
 	
 	
