7e484c9a4483a902266d642fd99eb969bf2598dd
Logan Kottler <logankottler@gmail.com>
2018-04-30 04:22:15 -0500

    top kek

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,21 @@
 package application;
 	
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -20,14 +24,12 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -42,10 +44,15 @@ public class Main extends Application {
 	
 	static MediaPlayer musicPlayer;
 	static ArrayList<Media> music = new ArrayList<Media>();
-	static String currentSong;
+	static Text cSongDisplay;
 	
 	@Override
 	public void start(Stage primaryStage) {
+		primaryStage.setResizable(false);
+		primaryStage.setMinWidth(frameWidth);
+		primaryStage.setMinHeight(frameHeight);
+		//primaryStage.setMaxWidth(frameWidth);
+		//primaryStage.setMaxHeight(frameHeight);
 		menuScreen(primaryStage);
 	}
 	
@@ -67,6 +74,7 @@ public class Main extends Application {
 		buildDefaults(menuPane);
 		Scene menuScene = new Scene(menuPane, frameWidth, frameHeight);
 		menuScene.getStylesheets().add("application/application.css");
+		System.out.println(menuScene.getHeight() + " " + menuScene.getWidth());
 		
 		int[] cData = new int[]{frameWidth / 2, frameHeight /2, 300};
 		Circle whiteP = new Circle();
@@ -77,16 +85,29 @@ public class Main extends Application {
 		
 		// pay no attention to this code.
 		Image secret = loadImage("menu.png");
-		ImageView imv = new ImageView(secret);
-		imv.setOpacity(0.008);
-		imv.setScaleX((cData[2] * 2) / (secret.getWidth()));
-		imv.setScaleY((cData[2] * 2) / (secret.getHeight()));
-		final HBox secretRegion = new HBox(imv);
-		secretRegion.setPrefSize(cData[2]*2, cData[2]*2);
+		ImageView imvS = new ImageView(secret);
+		imvS.setOpacity(0.010);
+		imvS.setScaleX((cData[2] * 2) / (secret.getWidth()));
+		imvS.setScaleY((cData[2] * 2) / (secret.getHeight()));
+		HBox secretRegion = new HBox(imvS);
 		secretRegion.setLayoutX(cData[0] - cData[2]+ 57);
 		secretRegion.setLayoutY(cData[1] - cData[2]+ 57);
 		
 		menuPane.getChildren().addAll(whiteP, secretRegion);
+		
+		Image wisconsin = loadImage("wi.png");
+		ImageView imvW = new ImageView(wisconsin);
+		imvW.setScaleX(1.1);
+		imvW.setScaleY(1.1);
+		HBox wiscoHBox = new HBox(imvW);
+		wiscoHBox.setLayoutX((frameWidth - wisconsin.getWidth()) / 2);
+		wiscoHBox.setLayoutY((frameHeight- wisconsin.getHeight())/ 2 + 25);
+		menuPane.getChildren().add(wiscoHBox);
+		
+		imvW.getStyleClass().add("image");
+		imvW.setOnMouseClicked(e -> viewBracket(primaryStage));
+		
+		
 		
 		Button bracketBtn = new Button("View Bracket");
 		bracketBtn.setOnAction(e -> {
@@ -97,8 +118,12 @@ public class Main extends Application {
 			viewBracket(primaryStage);
 		});
 		
+		
 		primaryStage.setScene(menuScene);
+		primaryStage.sizeToScene();
 		primaryStage.show();	
+
+		
 	}
 	
 	//Default styling (feel free to mess with these)
@@ -148,9 +173,11 @@ public class Main extends Application {
 	
 	public void viewBracket(Stage primaryStage) {
 		Pane root = new Pane();
+		buildDefaults(root);
 		Scene scene1 = new Scene(root, frameWidth, frameHeight);	
 		scene1.getStylesheets().add("application/application.css");
 
+		
 		ArrayList<Game> games = b.getGames();
 		
 		//Defaults (based around a 16 team bracket)
@@ -243,7 +270,9 @@ public class Main extends Application {
 	}
 	private static void loopMusic(String path){ loopMusic(music.indexOf(path));} //Overloaded
 	private static void loopMusic(int mIndex){
-		if (musicPlayer != null) musicPlayer.stop();
+		double currVol = 100;
+		String currentSong = "Hmm, something's not quite right...";
+		
 		Media songMedia = music.get(mIndex);
 		try {
 			currentSong = java.net.URLDecoder.decode(songMedia.getSource(), "UTF-8"); //decodes
@@ -253,50 +282,100 @@ public class Main extends Application {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(currentSong);
+		if (musicPlayer != null) {
+			musicPlayer.stop();
+			currVol = musicPlayer.getVolume();
+			musicPlayer.dispose();
+			System.out.println(currentSong);
+		}
+		
+		cSongDisplay.setText("Now Playing: " + currentSong);
 		musicPlayer = new MediaPlayer(music.get(mIndex));
-		musicPlayer.setOnEndOfMedia(new Runnable() {public void run(){musicPlayer.seek(Duration.ZERO);}} );
+		musicPlayer.setVolume(currVol);
+		musicPlayer.setOnEndOfMedia(new Runnable() {public void run(){loopMusic(true);}} );
 		musicPlayer.play();
 	}
 
 	private static void buildDefaults(Pane panel){ //This can add toolbars and such
 		HBox musicBar = new HBox(3);
+		HBox musicText = new HBox();
+		HBox musicButtons = new HBox(3);
+		
+		musicButtons.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+		musicButtons.setAlignment(Pos.BOTTOM_RIGHT);
+		musicButtons.setLayoutX(400);
+		musicButtons.setMinWidth(370);
+		
+		musicText.setMinWidth(frameWidth - 370);
+		
 		musicBar.setLayoutX(0);
-		musicBar.setLayoutY(frameHeight-25);
+		musicBar.setLayoutY(frameHeight - 25);
 		musicBar.setPrefWidth(frameWidth);
 		musicBar.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-		musicBar.setAlignment(Pos.BOTTOM_RIGHT);
+		musicBar.setAlignment(Pos.BOTTOM_LEFT);
 		musicBar.setMinHeight(25);
-		
-		Text cSongDisplay = new Text("Now Playing: " + currentSong);
-		cSongDisplay.setFont(Font.font("Verdana", 20));
 		
 		Button pSong = new Button("Previous Song");
 		pSong.setOnAction(e -> {
 			loopMusic(false);
-			cSongDisplay.setText("Now Playing: " + currentSong);
 		});
 		Button nSong = new Button("Next Song");
 		nSong.setOnAction(e -> {
 			loopMusic(true);
-			cSongDisplay.setText("Now Playing: " + currentSong);
 		});
-	
-		musicBar.getChildren().addAll(cSongDisplay, pSong, nSong);
+		
+		Text volText = new Text();
+		volText.setFont(Font.font("Verdana", 20));
+		volText.setFill(Color.WHITE);
+		Slider volumeSlider = new Slider();
+		volumeSlider.setOrientation(Orientation.HORIZONTAL);
+		volumeSlider.setPrefWidth(150);
+		volumeSlider.setShowTickMarks(true);
+		volumeSlider.setMajorTickUnit(10);
+		volumeSlider.setMinorTickCount(0);
+		volumeSlider.setShowTickLabels(false);
+		volumeSlider.valueProperty().addListener(
+				(observable, oldvalue, newvalue) ->
+				{
+					int i = newvalue.intValue();
+					volText.setText(Integer.toString(i) + "%");
+					musicPlayer.setVolume(i / 100.0);
+				});
+		volumeSlider.setValue(150);
+		
+		musicText.getChildren().add(cSongDisplay);
+		musicButtons.getChildren().addAll(volText, volumeSlider, pSong, nSong);
+		musicBar.getChildren().addAll(musicText, musicButtons);
 		panel.getChildren().add(musicBar);
 		
 	}
 	
 	public static void initVars(){
 		
+		cSongDisplay = new Text("Init");
+		cSongDisplay.setFont(Font.font("Verdana", 20));
+		
+		//Animate the text
+		double availWidth = frameWidth-230;
+		double msgWidth = 700;
+		KeyValue initKeyValue = new KeyValue(cSongDisplay.translateXProperty(), availWidth);
+		KeyFrame initFrame = new KeyFrame(Duration.ZERO, initKeyValue);
+		
+		KeyValue endKeyValue = new KeyValue(cSongDisplay.translateXProperty(), -1.0 * msgWidth);
+		KeyFrame endFrame = new KeyFrame(Duration.seconds(7), endKeyValue);
+		
+		Timeline timeline = new Timeline(initFrame, endFrame);
+		
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.play();
+		
 		File musicFolder = new File("res/snd/music");
 		File[] musicFiles = musicFolder.listFiles();
 		
 		for (File f : musicFiles){
 			music.add(new Media(f.toURI().toString()));
-			System.out.println(f.toURI().toString());
 		}
-		loopMusic(1);
+		loopMusic(2);
 		
 		int tempTeams = 32;
 		String[] teamNames = new String[tempTeams];
