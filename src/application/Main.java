fe9a1fc4c7be026d680372afceea74cbb19e73f2
Logan Kottler <lkottler@wisc.edu>
2018-05-03 11:24:50 -0500

    removed warning

--- src/application/Main.java
+++ src/application/Main.java
@@ -25,7 +25,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
